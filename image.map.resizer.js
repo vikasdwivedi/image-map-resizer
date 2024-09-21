fe956a3975fed4715c336c2c6400da3a42a5655e
Vikas Dwivedi <vik.dwivedi30@gmail.com>
2024-09-21 13:27:02 +0530

    [refactor] Refactoring the lib to autotrigger the event on page load and resize events

--- image.map.resizer.js
+++ image.map.resizer.js
@@ -1,43 +1,62 @@
 /*
-* Copyright (c) 2018 Vikas Dwivedi
+* Copyright (c) 2034 Vikas Dwivedi
 * Licensed under the MIT license
 */
 
-(function (global) {
- 
-	window.rwdImageMap = function () {
-	var	imagesWithMaps = global.document.querySelectorAll('img[usemap]');
-	for(var image of imagesWithMaps)
-	{
-		var tempImage =  new Image();
-			tempImage.src = image.src;
-			tempImage.onload = function () {
-				let tempWidth =  this.width,
-				tempHeight = this.height,
-				wPercent = image.width /100,
-				hPercent = image.height/100,
-				map = image.getAttribute('usemap').replace('#', ''),
-				cname = 'ogcoords',
-				imgMapAreas =  global.document.querySelectorAll('map[name="' + map + '"] > area');
-				
-				for(imgArea of imgMapAreas)
-				{
-				 	imgArea.getAttribute(cname) || imgArea.setAttribute('ogcoords', imgArea.getAttribute('coords'));
-					if(imgArea.getAttribute(cname))
-					{
-						let coords = imgArea.getAttribute(cname).split(','),
-						cpercent = new Array(coords.length);
-						for(let i = 0; i <coords.length; i++)
-						{
-							cpercent[i] = (i % 2 == 0) ? parseInt(((coords[i] / tempWidth) * 100) * wPercent) :
-							parseInt(((coords[i] / tempHeight) * 100) * hPercent);
-						}
-						imgArea.setAttribute('coords', cpercent.toString());
-					}	
-				}			
-			};
- }
-}
-})(window);
+(function() {
+	/**
+	 * Resize image map given an image element.
+	 *
+	 * @param {HTMLImageElement} img - Image element.
+	 */
+	function resizeImageMap(img) {
+		// useMap property indicates if image has an associated image map.
+		if (typeof img.useMap === 'string' && img.useMap !== '') {
+			var map = document.querySelector('map[name="' + img.useMap.split('#')[1] + '"]');
+			
+			// Continue only if map exists.
+			if (map) {
+				var originalWidth = img.getAttribute('orgWidth');
+				var originalHeight = img.getAttribute('orgHeight');
 
-window.addEventListener('resize', rwdImageMap);
+				// Continue only if both original width and height are provided.
+				if (originalWidth && originalHeight) {
+					var areas = map.getElementsByTagName('area');
+
+					for (var i = 0; i < areas.length; ++i) {
+						var originalCoords = areas[i].getAttribute('coords');
+
+						// Continue only if original coordinates are provided. 
+						if (originalCoords) {
+							var coords = originalCoords.split(',').map(function(coord) {
+								return Math.round(parseFloat(coord) * (coord.indexOf('%') !== -1 ? 0.01 : 1));
+							});
+							
+							areas[i].coords = coords.map(function(coord, index) {
+								// Scale coordinate using ratio of current width/height to original width/height.
+								return coord * (index % 2 === 0 ? img.offsetWidth / originalWidth : img.offsetHeight / originalHeight);
+							}).join(',');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Add event listener for `load` event.
+	window.addEventListener('load', function() {
+		// Get all the image elements.
+		var imgs = document.getElementsByTagName('img');
+		
+		for (var i = 0; i < imgs.length; ++i) {
+			resizeImageMap(imgs[i]);
+		}
+
+		// Add event listener for `resize` event.
+		window.addEventListener('resize', function() {
+			for (var i = 0; i < imgs.length; ++i) {
+				resizeImageMap(imgs[i]);
+			}
+		});
+	});
+})();
